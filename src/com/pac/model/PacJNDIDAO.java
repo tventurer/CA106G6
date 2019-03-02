94ca4b08f9cbc4c973c986fa6b0db1aea731596b
liauyu211 <liauyu211@gmail.com>
2019-03-02 18:18:28 +0800

    0302

--- src/com/pac/model/PacJNDIDAO.java
+++ src/com/pac/model/PacJNDIDAO.java
@@ -1,8 +1,11 @@
 package com.pac.model;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,30 +13,41 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Test
  */
-@WebServlet("/Test")
+
 public class PacJNDIDAO extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
-    public PacJNDIDAO(){
-        super();
-    }
-
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PacDAOimpl pacJDAO =new PacDAOimpl();
 		PacVO pacVO1 = new PacVO();
-		pacVO1.setPacno("PAC000004");
+		
+		byte[] b = null;
+		try {
+			File file = new File("C:\\Users\\Java\\Desktop\\images\\002.jpg");
+			InputStream in = new FileInputStream(file);
+			b = new byte[in.available()];
+			System.out.println(b.length);
+			in.read(b);
+			in.close();
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+		
+//		pacVO1.setPacno("PAC000004");
 		pacVO1.setEmpno("EMP000003");
 		pacVO1.setPacname("東京春爛漫~");
 		pacVO1.setPaccountry("日本");
 		pacVO1.setPaccity("東京");
 		pacVO1.setPactotalday(5);
-		pacVO1.setPacprice(28800);
-		pacVO1.setPacdeposit(5000);
+		pacVO1.setPacprice(88800);
+		pacVO1.setPacdeposit(85000);
 		pacVO1.setPacdiv("溫泉、城市");
 		pacVO1.setPaccontent("櫻花、溫泉，城市風光");
 		pacVO1.setPactchar1(null);
-		pacVO1.setPactchar2(null);
+		pacVO1.setPactchar2(b);
 		pacVO1.setPacremark("");
 		pacVO1.setPacstatus(2);
 		pacJDAO.insert(pacVO1);			
