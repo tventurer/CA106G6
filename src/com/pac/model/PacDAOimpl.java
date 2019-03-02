2c7137c60a16018eade78ce25ff040bc432f27ea
liauyu211 <liauyu211@gmail.com>
2019-03-02 18:21:00 +0800

    完整

--- src/com/pac/model/PacDAOimpl.java
+++ src/com/pac/model/PacDAOimpl.java
@@ -1,5 +1,10 @@
 package com.pac.model;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,7 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
- 
+
 public class PacDAOimpl implements PacDAO_interface{
 	String driver = "oracle.jdbc.driver.OracleDriver";
 	String url = "jdbc:oracle:thin:@localhost:1521:XE";
@@ -25,10 +30,8 @@ public class PacDAOimpl implements PacDAO_interface{
 		private static final String DELETE = 
 			"DELETE FROM PACKAGE WHERE PACNO = ?";
 		private static final String UPDATE = 
-			"UPDATE PACKAGE SET PACNAME=?, PACCITY=?, PACPRICE=?, PACSTATUS=? WHERE PACNO = ?";
+			"UPDATE PACKAGE SET EMPNO=? ,PACNAME=? ,PACCOUNTRY=? ,PACCITY=? ,PACTOTALDAY=? ,PACPRICE=? ,PACDEPOSIT=? ,PACDIV=? ,PACCONTENT=? ,PACTCHAR1=? ,PACTCHAR2=? ,PACREMARK=? ,PACSTATUS=? WHERE PACNO = ?";
 
-	
-		
 			@Override
 			public void insert(PacVO pacVO) {
 
@@ -97,28 +100,22 @@ public class PacDAOimpl implements PacDAO_interface{
 
 					Class.forName(driver);
 					con = DriverManager.getConnection(url, userid, passwd);
+					
 					pstmt = con.prepareStatement(UPDATE);
-//					原版
-//					pstmt.setString(1, pacVO.getPacno());
-//					pstmt.setString(2, pacVO.getEmpno());
-//					pstmt.setString(3, pacVO.getPacname());
-//					pstmt.setString(4, pacVO.getPaccountry());
-//					pstmt.setString(5, pacVO.getPaccity());
-//					pstmt.setInt(6, pacVO.getPactotalday());
-//					pstmt.setInt(7, pacVO.getPacprice());
-//					pstmt.setInt(8, pacVO.getPacdeposit());
-//					pstmt.setString(9, pacVO.getPacdiv());
-//					pstmt.setString(10, pacVO.getPaccontent());
-//					pstmt.setBytes(11, pacVO.getPactchar1());
-//					pstmt.setBytes(12, pacVO.getPactchar2());
-//					pstmt.setString(13, pacVO.getPacremark());
-//					pstmt.setInt(14, pacVO.getPacstatus());
-					//"UPDATE PACKAGE SET PACNAME=?, PACCITY=?, PACPRICE=?, PACSTATUS=? WHERE PACNO = ?";
-					pstmt.setString(1, pacVO.getPacname());
-					pstmt.setString(2, pacVO.getPaccity());
-					pstmt.setInt(3, pacVO.getPacprice());
-					pstmt.setInt(4, pacVO.getPacstatus());
-					pstmt.setString(5, pacVO.getPacno());
+					pstmt.setString(1, pacVO.getEmpno());
+					pstmt.setString(2, pacVO.getPacname());
+					pstmt.setString(3, pacVO.getPaccountry());
+					pstmt.setString(4, pacVO.getPaccity());
+					pstmt.setInt(5, pacVO.getPactotalday());
+					pstmt.setInt(6, pacVO.getPacprice());
+					pstmt.setInt(7, pacVO.getPacdeposit());
+					pstmt.setString(8, pacVO.getPacdiv());
+					pstmt.setString(9, pacVO.getPaccontent());
+					pstmt.setBytes(10, pacVO.getPactchar1());
+					pstmt.setBytes(11, pacVO.getPactchar2());
+					pstmt.setString(12, pacVO.getPacremark());
+					pstmt.setInt(13, pacVO.getPacstatus());
+					pstmt.setString(14, pacVO.getPacno());
 					pstmt.executeUpdate();
 
 					// Handle any driver errors
@@ -344,34 +341,46 @@ public class PacDAOimpl implements PacDAO_interface{
 				PacDAOimpl dao = new PacDAOimpl();
 
 				// 新增
-				PacVO pacVO1 = new PacVO();
+//				PacVO pacVO1 = new PacVO();
 				
 //				pacVO1.setPacno("PAC000007");
-				pacVO1.setEmpno("EMP000002");
-				pacVO1.setPacname("東京春爛漫~");
-				pacVO1.setPaccountry("日本");
-				pacVO1.setPaccity("東京");
-				pacVO1.setPactotalday(5);
-				pacVO1.setPacprice(28800);
-				pacVO1.setPacdeposit(5000);
-				pacVO1.setPacdiv("溫泉、城市");
-				pacVO1.setPaccontent("櫻花、溫泉，城市風光");
-				pacVO1.setPactchar1(null);
-				pacVO1.setPactchar2(null);
-				pacVO1.setPacremark("");
-				pacVO1.setPacstatus(2);
-				dao.insert(pacVO1);		
-				System.out.println("新增一筆"+ pacVO1.getPacno());
-//				
-//				
-//				
-////
-//				// 修改
+//				pacVO1.setEmpno("EMP000002");
+//				pacVO1.setPacname("東京春爛漫~");
+//				pacVO1.setPaccountry("日本");
+//				pacVO1.setPaccity("東京");
+//				pacVO1.setPactotalday(5);
+//				pacVO1.setPacprice(28800);
+//				pacVO1.setPacdeposit(5000);
+//				pacVO1.setPacdiv("溫泉、城市");
+//				pacVO1.setPaccontent("櫻花、溫泉，城市風光");
+//				pacVO1.setPactchar1(null);
+//				pacVO1.setPactchar2(null);
+//				pacVO1.setPacremark("");
+//				pacVO1.setPacstatus(2);
+//				dao.insert(pacVO1);		
+//				System.out.println("新增成功"+ pacVO1.getPacno());
+				
+				
+				System.out.println("============================");
+				// 修改
 				PacVO pacVO2 = new PacVO();
-		/*		所有原來的
-		 *  	pacVO2.setPacno("PAC000009");
+		/*	* 	所有原來的*/
+				
+				byte[] b = null;
+				try {
+					File file = new File("D:\\001.jpg");
+					InputStream in = new FileInputStream(file);
+					b = new byte[in.available()];
+					in.read(b);
+					in.close();
+				}catch(FileNotFoundException e) {
+					e.printStackTrace();
+				}catch(IOException ioe) {
+					ioe.printStackTrace();
+				}
+				
 				pacVO2.setEmpno("EMP000003");
-				pacVO2.setPacname("東京熱");
+				pacVO2.setPacname("東京熱123");
 				pacVO2.setPaccountry("日本XXX");
 				pacVO2.setPaccity("東京花街");
 				pacVO2.setPactotalday(5);
@@ -379,16 +388,17 @@ public class PacDAOimpl implements PacDAO_interface{
 				pacVO2.setPacdeposit(5000);
 				pacVO2.setPacdiv("溫泉、城市");
 				pacVO2.setPaccontent("櫻花、溫泉，城市風光");
-				pacVO2.setPactchar1(null);
+				pacVO2.setPactchar1(b);
 				pacVO2.setPactchar2(null);
-				pacVO2.setPacremark("");
-				pacVO2.setPacstatus(2);
-		 * */		
-				pacVO2.setPacname("東京熱");
-				pacVO2.setPaccity("東京花街");
-				pacVO2.setPacprice(28800);
+				pacVO2.setPacremark("注意事項");
 				pacVO2.setPacstatus(2);
 				pacVO2.setPacno("PAC000007");
+		 		
+//				pacVO2.setPacname("東京熱");
+//				pacVO2.setPaccity("東京花街");
+//				pacVO2.setPacprice(28800);
+//				pacVO2.setPacstatus(2);
+//				pacVO2.setPacno("PAC000007");
 				dao.update(pacVO2);
 
 //
@@ -397,36 +407,36 @@ public class PacDAOimpl implements PacDAO_interface{
 //				dao.delete("PAC000006");
 
 				// 查詢
-				PacVO pacVO3 = dao.findByPrimaryKey("PAC000002");
-				System.out.print(pacVO3.getPacname() + ",");
-				System.out.print(pacVO3.getPaccountry() + ",");
-				System.out.print(pacVO3.getPactotalday() + ",");
-				System.out.print(pacVO3.getPacprice() + ",");
-				System.out.print(pacVO3.getPacdeposit() + ",");
-				System.out.print(pacVO3.getPaccontent() + ",");
-				System.out.print(pacVO3.getPacremark()+ ",");
-				System.out.println(pacVO3.getPacstatus() + " ---");
-				System.out.println("---------------------");
-
-				// 查詢
-				List<PacVO> list = dao.getAll();
-				for (PacVO aEmp : list) {
-					System.out.print(aEmp.getPacno() + ",");
-					System.out.print(aEmp.getEmpno() + ",");
-					System.out.print(aEmp.getPacname() + ",");
-					System.out.print(aEmp.getPaccountry() + ",");
-					System.out.print(aEmp.getPaccity() + ",");
-					System.out.print(aEmp.getPactotalday() + ",");
-					System.out.print(aEmp.getPacprice());
-					System.out.print(aEmp.getPacdeposit() + ",");
-					System.out.print(aEmp.getPacdiv() + ",");
-					System.out.print(aEmp.getPaccontent() + ",");
-					System.out.print(aEmp.getPactchar1() + ",");
-					System.out.print(aEmp.getPactchar2() + ",");
-					System.out.print(aEmp.getPacremark() + ",");
-					System.out.print(aEmp.getPacstatus() + "  ---");
-					System.out.println();
-				}
+//				PacVO pacVO3 = dao.findByPrimaryKey("PAC000002");
+//				System.out.print(pacVO3.getPacname() + ",");
+//				System.out.print(pacVO3.getPaccountry() + ",");
+//				System.out.print(pacVO3.getPactotalday() + ",");
+//				System.out.print(pacVO3.getPacprice() + ",");
+//				System.out.print(pacVO3.getPacdeposit() + ",");
+//				System.out.print(pacVO3.getPaccontent() + ",");
+//				System.out.print(pacVO3.getPacremark()+ ",");
+//				System.out.println(pacVO3.getPacstatus() + " ---");
+//				System.out.println("---------------------");
+//
+//				// 查詢
+//				List<PacVO> list = dao.getAll();
+//				for (PacVO aEmp : list) {
+//					System.out.print(aEmp.getPacno() + ",");
+//					System.out.print(aEmp.getEmpno() + ",");
+//					System.out.print(aEmp.getPacname() + ",");
+//					System.out.print(aEmp.getPaccountry() + ",");
+//					System.out.print(aEmp.getPaccity() + ",");
+//					System.out.print(aEmp.getPactotalday() + ",");
+//					System.out.print(aEmp.getPacprice());
+//					System.out.print(aEmp.getPacdeposit() + ",");
+//					System.out.print(aEmp.getPacdiv() + ",");
+//					System.out.print(aEmp.getPaccontent() + ",");
+//					System.out.print(aEmp.getPactchar1() + ",");
+//					System.out.print(aEmp.getPactchar2() + ",");
+//					System.out.print(aEmp.getPacremark() + ",");
+//					System.out.print(aEmp.getPacstatus() + "  ---");
+//					System.out.println();
+//				}
 			}
 		}
 	
