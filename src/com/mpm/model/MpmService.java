ad1a93c639a4d2098bb963f881bab371bdf0dada
Hsieh <gasbomb_tw@hotmail.com>
2019-03-22 07:54:28 +0800

    會員訊息service修改

--- src/com/mpm/model/MpmService.java
+++ src/com/mpm/model/MpmService.java
@@ -9,6 +9,17 @@ public class MpmService {
 		dao = new MpmDAO();
 	}
 	
+	public MpmVO addMpm(String memsender, String mpmreceiver, String mpmtitle, String mpmcontent) {
+		MpmVO mpmVO = new MpmVO();
+		mpmVO.setMpmsender(memsender);
+		mpmVO.setMpmreceiver(mpmreceiver);
+		mpmVO.setMpmtitle(mpmtitle);
+		mpmVO.setMpmcontent(mpmcontent);
+		dao.insert(mpmVO);
+		
+		return mpmVO;
+	}
+	
 	public List<MpmVO> getAll() {
 		return dao.getAll();
 	}
