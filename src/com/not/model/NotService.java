315527a446544e88625b621869115466b2ae3331
galaxian85 <47630639+galaxian85@users.noreply.github.com>
2019-03-22 15:56:12 +0800

    Update NotService.java

--- src/com/not/model/NotService.java
+++ src/com/not/model/NotService.java
@@ -1,12 +1,22 @@
 package com.not.model;
 
 import java.util.List;
+import com.not.model.NotVO;
 
 public class NotService {
 	private NotDAO_interface dao;
 	
 	public NotService() {
 		dao = new NotDAO();
+	}
+	
+	public NotVO addNot(String memno, String notcontent) {
+		NotVO notVO = new NotVO();
+		notVO.setMemno(memno);
+		notVO.setNotcontent(notcontent);
+		dao.insert(notVO);
+		
+		return notVO;
 	}
 	
 	public List<NotVO> getByMemno(String memno) {
